94eb009120909a557608f28be9d587be01d36e75
jaspreet <ghotra.jaspreet003@gmail.com>
2024-01-29 11:16:31 -0500

    added different pages and their own template

--- template-contactus.php
+++ template-contactus.php
@@ -5,5 +5,29 @@ Template Name: Contact Us
 */
 
 ?>
+<?php get_header('secondary');?>
 
-This is contact us page template
+
+<div class="container">
+
+	<h1><?php the_title();?></h1>
+
+
+	<div class="row">
+		
+			<div class="col-lg-6">
+				
+				This is where the contact form goes
+			</div>
+
+			<div class="col-lg-6">
+				
+				<?php get_template_part('includes/section','content');?>
+			</div>
+
+
+	</div>
+
+</div>	
+
+<?php get_footer();?>
