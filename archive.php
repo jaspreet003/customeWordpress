fd50fec8ad668003ef1348a0adec980dc8414cc2
jaspreet <ghotra.jaspreet003@gmail.com>
2024-01-31 15:29:51 -0500

    blog page different features

--- archive.php
+++ archive.php
@@ -4,10 +4,31 @@
 
 <div class="container">
 
-<h1><?php the_title();?></h1>
+<h1><?php single_cat_title();?></h1>
 
-<?php get_template_part('includes/section','content');?>
-	
+<?php get_template_part('includes/section','archieve');?>
+
+<?php previous_posts_link();?>
+<?php next_posts_link();?>
+
+<?php
+/*
+//****** this is pagenated way to add next previous with page numbers *****
+
+
+global $wp_query;
+
+$big = 999999999; //need an unlikely integer
+
+echo paginate_links(array(
+
+	'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+	'format'=> '?paged=%#%',
+	'current'=> max(1, get_query_var('paged')),
+	'total' => $wp_query->max_num_pages 
+) );
+*/
+?>
 
 
 </div>
