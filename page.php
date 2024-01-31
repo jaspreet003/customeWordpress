29043e3579c6c0ede72a71c76434491ad247c5a5
jaspreet <ghotra.jaspreet003@gmail.com>
2024-01-31 15:30:33 -0500

    blof files

--- page.php
+++ page.php
@@ -6,6 +6,12 @@
 
 <h1><?php the_title();?></h1>
 
+<?php if(has_post_thumbnail()):?>
+
+		<img src="<?php the_post_thumbnail_url('blog-large');?>" alt="<?php the_title();?>" class="img-fluid mb-3 img-thumbnail mr-4">
+
+<?php endif;?>
+
 <?php get_template_part('includes/section','content');?>
 	
 
